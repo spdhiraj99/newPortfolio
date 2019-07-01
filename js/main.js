586ba3408a4a320f243d58deda834f107b9ac1e2
root <root@localhost.localdomain>
2019-07-01 22:19:21 +0530

    done till skills

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+//scroll up show navbar
+$(document).ready(function(){
+    var previousScroll = 0;
+    $(window).scroll(function(){
+      var currentScroll = $(this).scrollTop();
+      if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()){
+        if (currentScroll > previousScroll){
+          window.setTimeout(hideNav, 300);
+        } else {
+          window.setTimeout(showNav, 300);
+        }
+        previousScroll = currentScroll;
+      }
+  
+    });
+  
+    function hideNav() {
+      $("[data-nav-status='toggle']").removeClass("is-visible").addClass("is-hidden");
+    }
+    function showNav() {
+      $("[data-nav-status='toggle']").removeClass("is-hidden").addClass("is-visible");
+    }
+});
+//toggle navbar
 (function ($) {
 	"use strict";
 	var nav = $('nav');
@@ -94,7 +118,7 @@
 // ——————————————————————————————————————————————————
 var top0 = $('#home').offset().top;
 var top1 = $('#about').offset().top;
-var top2 = $('#services').offset().top;
+var top2 = $('#skills').offset().top;
 var top3 = $('#work').offset().top;
 
 $(document).scroll(function() {
