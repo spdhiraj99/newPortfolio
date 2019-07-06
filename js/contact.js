78e1c1db92ce3eb432b95e6716663a6beec5bcd7
root <root@localhost.localdomain>
2019-07-07 02:02:48 +0530

    just contact left

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,15 @@
-$.get('https://dhiraj-mail.000webhostapp.com/mailer.php', function(responseText) {
-    alert(responseText);
-});
+function SubmitFormData() {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var subject = $("#subject").val();
+    var message = $("#message").val();
+    $.post("https://dhiraj-mail.000webhostapp.com/mailer.php", { name: name, email: email, subject:subject, message:message },
+    function(responseText) {
+     alert(responseText);
+     console.log(responseText);
+    });
+}
+
+// $.get('https://dhiraj-mail.000webhostapp.com/mailer.php', function(responseText) {
+//     alert(responseText);
+// });
--- server/mailer.php
+++ server/mailer.php
@@ -1,9 +1,13 @@
 <?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = "<h1>Sent by ${name}, ${email} </h1><br>";
+    $message .= $_POST['message'];
     $to = "spdhiraj99@gmail.com";
-    $subject = "This is subject";     
-    $message = "<b>This is HTML message.</b>";
-    $message .= "<h1>This is headline.</h1>";      
-    $retval = mail ($to,$subject,$message);     
+    $header = "MIME-Version: 1.0\r\n";
+    $header .= "Content-type: text/html\r\n"; 
+    $retval = mail ($to,$subject,$message,$header);     
     if( $retval == true ) {
         echo "Message sent successfully...";
      }else {
